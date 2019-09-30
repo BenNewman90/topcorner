b83acdd5155bc41846500c764651fe1f5dcd673e
Ben Newman <benjamin-newman@live.co.uk>
2019-09-30 20:05:58 +0100

    updated app to redirect user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {connect } from 'react-redux'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -8,24 +8,19 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
+import { setCurrentUser } from './redux/user/user.actions'
+
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
-
+        
         userRef.onSnapshot(snapshot => {
-            this.setState({
+            this.props.setCurrentUser({
               id: snapshot.id,
               ...snapshot.data()
             }, () =>{
@@ -34,7 +29,7 @@ class App extends React.Component {
         })
         
       }
-      this.setState({ currentUser: userAuth}, () => {console.log(this.state)}
+      setCurrentUser({ userAuth}, () => {console.log(this.state)}
       );
     })
   }
@@ -47,14 +42,32 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) =>({
+  currentUser: user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,21 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
-    Im the homepage
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium sit amet tortor eget luctus. Fusce pulvinar et lorem id tincidunt. Nulla porttitor eros ac purus laoreet, non congue nisi hendrerit. Vivamus vitae viverra ligula. Morbi ac diam nulla. Aenean tincidunt, felis eget convallis tristique, est urna tincidunt justo, in euismod odio elit a magna. Quisque pharetra, arcu in posuere sagittis, enim diam porta est, quis malesuada ligula lorem ac dui. Vivamus egestas, est in iaculis facilisis, diam eros hendrerit turpis, ut malesuada magna nibh a erat. Sed bibendum nisi est, id rutrum odio dignissim eu. In hac habitasse platea dictumst.
+      </p> 
+      <p>
+      Vivamus elit erat, commodo vel odio vel, finibus bibendum enim. Integer non arcu at magna aliquet mollis ac quis mauris. Ut ac iaculis justo. Praesent molestie, quam vulputate pretium efficitur, lorem ligula rhoncus nibh, vehicula pharetra sapien ligula non ante. Morbi a leo lectus. Sed sed elit ac libero lobortis egestas eget a orci. Fusce scelerisque id felis ut tincidunt. Integer vulputate turpis eget lorem pulvinar, in auctor lacus posuere. Nulla tincidunt est est, in blandit dolor convallis eu. Praesent dolor leo, sollicitudin in massa sed, consectetur dapibus est.
+      </p> 
+      <p>
+      Morbi malesuada imperdiet ex in tempus. Vivamus mattis quam in vulputate viverra. Suspendisse et leo et neque finibus laoreet. Vestibulum mattis orci aliquam congue sollicitudin. Phasellus euismod tellus quis tincidunt efficitur. Vestibulum lobortis velit sit amet viverra lacinia. In egestas nibh a hendrerit blandit.
+      </p> 
+      <p>
+      Nulla facilisi. Suspendisse ut imperdiet massa. Nunc pulvinar lectus sit amet facilisis pretium. Donec dapibus ultricies aliquet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris dignissim, nisl vitae sollicitudin faucibus, sapien velit blandit sapien, dignissim rhoncus turpis sem eu tortor. Aenean scelerisque, diam sit amet euismod convallis, eros ex sodales turpis, vel imperdiet erat eros sit amet quam. Phasellus ultricies maximus ornare.
+      </p> 
+      <p>
+      Donec varius ultricies aliquam. Nunc eu bibendum mauris. Etiam id massa sed dui dignissim sagittis. Sed porttitor mauris suscipit pellentesque blandit. Duis vel dictum nulla. Sed sit amet nulla pharetra, varius magna pulvinar, porttitor massa. Morbi eleifend ipsum enim, sollicitudin luctus risus mollis laoreet. Nam tristique, neque eu scelerisque volutpat, sem nisi eleifend ligula, a porttitor odio urna in ante. Integer imperdiet libero sed urna semper scelerisque. Donec vulputate lorem interdum, maximus arcu eu, sodales nulla. Etiam purus massa, vulputate in vestibulum quis, congue sit amet velit. Praesent et tempor neque. Donec lacinia justo ac iaculis finibus. Phasellus sed pulvinar ipsum. Quisque luctus ornare magna et ultrices.
+      </p> 
   </div>
 );
 
